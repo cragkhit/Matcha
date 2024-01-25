567d8d9dc1fe05e9158a8b7ef32db139dd7449df
gurapun <pun.sutanchaiyanonta@kingpower.com>
2024-01-25 22:33:20 +0700

    edit logic latest version v.3

--- src/main/java/crest/siamese/helpers/LatestResultVersionRetriever.java
+++ src/main/java/crest/siamese/helpers/LatestResultVersionRetriever.java
@@ -22,8 +22,7 @@ public class LatestResultVersionRetriever {
         String fileNameWithOutVersion = String.join("_", RemoveArrayByIndex(fileNames,fileNames.length-1));
         String fileVersion = fileNames[fileNames.length-1];
         if(fileVersion.equals("recent")){
-//            result.add(esResults.get(0));
-            return result;
+         return new  ArrayList<Document>();
         }
         String newestVersion = fileVersion;
         int newestNumberVersion = 0;
